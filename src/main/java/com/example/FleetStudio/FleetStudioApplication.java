105d4f01ef3002c91f18c02aabea98a16bd5451e
shivanarayana <shhivaa00@gmail.com>
2022-10-17 17:08:45 +0530

    Initial Files

---
+++ src/main/java/com/example/FleetStudio/FleetStudioApplication.java
@@ -0,0 +1,13 @@
+package com.example.FleetStudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetStudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FleetStudio/FleetStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FleetStudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetStudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
